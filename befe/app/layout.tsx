0a097da418f0bff7abb132ee232482d78179a7fa
centroidissue <iamnoob2210@gmail.com>
2025-02-13 19:22:52 +0800

    Add boilerplate

--- befe/app/layout.tsx
+++ befe/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
+
+
 import "./globals.css";
 
-const geistSans = Geist({
+
+
+const poppins = Poppins({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} body`}
       >
         {children}
       </body>
---
+++ befe/interfaces/NavigationContent.tsx
@@ -0,0 +1,5 @@
+
+export default interface NavigationContent {
+  name: string;
+  link: string;
+}
